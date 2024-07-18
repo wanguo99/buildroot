70a8fa6eac5146fec79090abb8cb79ff22f4f566
wanguo <guohaoprc@163.com>
2024-07-18 22:29:59 +0800

    Update system PATH

--- package/wgtool/wgtool.mk
+++ package/wgtool/wgtool.mk
@@ -1,6 +1,6 @@
 ################################################################################
 #
-# WGTOOL: wanguo test app
+# WGTOOL: wgtool test app
 #
 ################################################################################
 
@@ -12,11 +12,11 @@ WGTOOL_SITE = $(TOPDIR)/package/wgtool/src
 WGTOOL_SITE_METHOD = local
 WGTOOL_TARGET_APP = wgtool
 
-define WG_TEST_BUILD_CMDS
+define WGTOOL_BUILD_CMDS
     $(MAKE) -C $(@D) CROSS_COMPILE=$(TARGET_CROSS)
 endef
 
-define WG_TEST_INSTALL_TARGET_CMDS
+define WGTOOL_INSTALL_TARGET_CMDS
     $(INSTALL) -d -m 0755 $(TARGET_DIR)/usr/local/bin
     $(INSTALL) -D -m 0755 $(@D)/$(WGTOOL_TARGET_APP) $(TARGET_DIR)/usr/local/bin/
 endef

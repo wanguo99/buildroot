12263900a8a35ee880bac2ddab7a4587186f4cf2
isGuoHao <guohaoprc@163.com>
2024-07-21 19:53:20 +0800

    Add led hw drv opts

--- package/pdl/src/led/pdl_led.c
+++ package/pdl/src/led/pdl_led.c
@@ -1,3 +1,6 @@
+#include <linux/io.h>
+#include <linux/delay.h>
+
 #include "osa/osa.h"
 #include "pdl.h"
 #include "pdl_led_priv.h"
@@ -15,6 +18,131 @@ static DEFINE_MUTEX(g_stLedMutex);
 static bool led_is_on = false;
 static bool led_is_blinking = false;
 
+
+#define LEDOFF 	0				/* 关灯 */
+#define LEDON 	1				/* 开灯 */
+
+/* 寄存器物理地址 */
+#define CCM_CCGR1_BASE				(0X020C406C)
+#define SW_MUX_GPIO1_IO03_BASE		(0X020E0068)
+#define SW_PAD_GPIO1_IO03_BASE		(0X020E02F4)
+#define GPIO1_DR_BASE				(0X0209C000)
+#define GPIO1_GDIR_BASE				(0X0209C004)
+
+/* 映射后的寄存器虚拟地址指针 */
+static void __iomem *IMX6U_CCM_CCGR1;
+static void __iomem *SW_MUX_GPIO1_IO03;
+static void __iomem *SW_PAD_GPIO1_IO03;
+static void __iomem *GPIO1_DR;
+static void __iomem *GPIO1_GDIR;
+
+/*************************************************************************/
+/*                              LED驱动接口                                  */
+/*************************************************************************/
+static INT32 LED_drv_on(VOID)
+{
+	UINT32 val = 0;
+
+    OSA_INFO("");
+
+    val = readl(GPIO1_DR);
+    val &= ~(1 << 3);
+    writel(val, GPIO1_DR);
+
+    return OSA_SOK;
+}
+
+static INT32 LED_drv_off(VOID)
+{
+	UINT32 val = 0;
+
+    OSA_INFO("");
+    val = readl(GPIO1_DR);
+    val|= (1 << 3);
+    writel(val, GPIO1_DR);
+
+    return OSA_SOK;
+}
+
+static INT32 LED_drv_blink(VOID)
+{
+    OSA_INFO("");
+
+    (VOID)LED_drv_on();
+
+    msleep(500);
+
+    (VOID)LED_drv_off();
+
+    msleep(500);
+
+    (VOID)LED_drv_on();
+
+    msleep(500);
+
+    (VOID)LED_drv_off();
+
+    msleep(500);
+
+    (VOID)LED_drv_on();
+
+    msleep(500);
+
+    (VOID)LED_drv_off();
+
+    return OSA_SOK;
+}
+
+
+/* 初始化硬件管脚 */
+static INT32 LED_drv_setup(VOID)
+{
+	UINT32 val = 0;
+
+	/* 初始化LED */
+	/* 1、寄存器地址映射 */
+  	IMX6U_CCM_CCGR1 = ioremap(CCM_CCGR1_BASE, 4);
+	SW_MUX_GPIO1_IO03 = ioremap(SW_MUX_GPIO1_IO03_BASE, 4);
+  	SW_PAD_GPIO1_IO03 = ioremap(SW_PAD_GPIO1_IO03_BASE, 4);
+	GPIO1_DR = ioremap(GPIO1_DR_BASE, 4);
+	GPIO1_GDIR = ioremap(GPIO1_GDIR_BASE, 4);
+
+	/* 2、使能GPIO1时钟 */
+	val = readl(IMX6U_CCM_CCGR1);
+	val &= ~(3 << 26);	/* 清楚以前的设置 */
+	val |= (3 << 26);	/* 设置新值 */
+	writel(val, IMX6U_CCM_CCGR1);
+
+	/* 3、设置GPIO1_IO03的复用功能，将其复用为
+	 *    GPIO1_IO03，最后设置IO属性。
+	 */
+	writel(5, SW_MUX_GPIO1_IO03);
+
+	/*寄存器SW_PAD_GPIO1_IO03设置IO属性
+	 *bit 16:0 HYS关闭
+	 *bit [15:14]: 00 默认下拉
+     *bit [13]: 0 kepper功能
+     *bit [12]: 1 pull/keeper使能
+     *bit [11]: 0 关闭开路输出
+     *bit [7:6]: 10 速度100Mhz
+     *bit [5:3]: 110 R0/6驱动能力
+     *bit [0]: 0 低转换率
+	 */
+	writel(0x10B0, SW_PAD_GPIO1_IO03);
+
+	/* 4、设置GPIO1_IO03为输出功能 */
+	val = readl(GPIO1_GDIR);
+	val &= ~(1 << 3);	/* 清除以前的设置 */
+	val |= (1 << 3);	/* 设置为输出 */
+	writel(val, GPIO1_GDIR);
+
+	/* 5、默认关闭LED */
+	val = readl(GPIO1_DR);
+	val |= (1 << 3);
+	writel(val, GPIO1_DR);
+
+    return OSA_SOK;
+}
 
 /*************************************************************************/
 /*                              字符设备文件                                   */
@@ -128,6 +256,8 @@ static VOID LED_proc_on(VOID)
 {
     OSA_print("o: Open the led.");
 
+    (VOID)LED_drv_on();
+
     return;
 }
 
@@ -135,12 +265,16 @@ static VOID LED_proc_off(VOID)
 {
     OSA_print("c: Close the led.");
 
+    (VOID)LED_drv_off();
+
     return;
 }
 
 static VOID LED_proc_blink(VOID)
 {
     OSA_print("b: Set the led blink.");
+
+    (VOID)LED_drv_blink();
 
     return;
 }
@@ -259,6 +393,13 @@ INT32 PDL_LED_init(VOID)
     if (OSA_isNotSuccess(iRet))
     {
         OSA_ERROR("OSA_proc_create failed.");
+        return OSA_EFAIL;
+    }
+
+    iRet = LED_drv_setup();
+    if (OSA_isNotSuccess(iRet))
+    {
+        OSA_ERROR("LED_drv_setup failed.");
         return OSA_EFAIL;
     }
 

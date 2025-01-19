51065654f0e0bfbd5077860c0520e51ce7d0693f
wanguo <guohaoprc@163.com>
2025-01-19 11:23:28 +0800

    - Issues: libpdm support local/Git/svn source - Date: 2025/01/19 11:23:28 - Author: wanguo

--- package/libpdm/libpdm.mk
+++ package/libpdm/libpdm.mk
@@ -1,18 +1,31 @@
 ################################################################################
 #
-# libpdm
+# PDM (Perpherial Driver Module) Library
 #
 ################################################################################
 
-LIBPDM_VERSION = $(call qstrip,$(BR2_PACKAGE_LIBPDM_GIT_REPO_VERSION))
-LIBPDM_SITE = $(call qstrip,$(BR2_PACKAGE_LIBPDM_GIT_REPO_URL))
-LIBPDM_SITE_METHOD = git
+LIBPDM_VERSION = $(call qstrip,$(BR2_PACKAGE_LIBPDM_VERSION))
+# Specify the license and where to find the license file(s)
 LIBPDM_LICENSE = GPL-2.0
 LIBPDM_LICENSE_FILES = LICENSE
 
+# Compute PDM_SOURCE and PDM_SITE from the configuration
+ifeq ($(BR2_PACKAGE_LIBPDM_CUSTOM_LOCAL_DIRECTORY),y)
+LIBPDM_SITE = $(call qstrip,$(BR2_PACKAGE_LIBPDM_CUSTOM_LOCAL_DIRECTORY_LOCATION))
+LIBPDM_SITE_METHOD = local
+else ifeq ($(BR2_PACKAGE_LIBPDM_CUSTOM_GIT),y)
+LIBPDM_SITE = $(call qstrip,$(BR2_PACKAGE_LIBPDM_CUSTOM_REPO_URL))
+LIBPDM_SITE_METHOD = git
+else # default using git
+LIBPDM_SITE = $(call qstrip,$(BR2_PACKAGE_LIBPDM_CUSTOM_REPO_URL))
+LIBPDM_SITE_METHOD = git
+endif
 
-LIBPDM_CONF_OPTS += \
-	-DBUILD_APPS=OFF \
-	-DCMAKE_CXX_FLAGS="$(TARGET_CXXFLAGS) -std=c++11 -fPIC -DAP4_PLATFORM_BYTE_ORDER=$(LIBPDM_BYTE_ORDER)"
+ifeq ($(LIBPDM_SITE),)
+$(error BR2_PACKAGE_LIBPDM_VERSION: $(BR2_PACKAGE_LIBPDM_VERSION))
+$(error LIBPDM_SITE: $(LIBPDM_SITE))
+$(error LIBPDM_SITE_METHOD: $(LIBPDM_SITE_METHOD))
+$(error No custom kernel source set. Check your BR2_PACKAGE_LIBPDM_VERSION setting)
+endif
 
 $(eval $(cmake-package))
